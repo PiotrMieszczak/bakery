dca1e5d8139f2ce31d028c801df0c40205d0a51b
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-18 14:30:19 +0200

    added font_awesome, mobile menu styles

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,33 @@
 $(document).ready(function(){
 
+//SLIDER
     var slider = $('.slider')
     var animationSpeed = 2000; //ms
     var currentSlide = 1;
-    console.log();
 
     var firstLi = slider.children().eq(0);
     var cloneFirstLi = firstLi.clone();
     slider.append(cloneFirstLi);
+    var intervalId;
 
-    var intervalId = setInterval(function(){
+    function startSlider(){
+        intervalId = setInterval(function(){
         slider.animate({'margin-left': '-=100%'},animationSpeed, function(){
             currentSlide++;
             if(currentSlide === slider.children().length){  
                 slider.css('margin-left',0);
                  currentSlide = 1;
-            }
-        });
-    }, 4000)
+                }
+            });
+        }, 4000)
+    }
+
+    function stopSlider(){
+        clearInterval(intervalId);
+    }
+
+    startSlider();
+
+//Mobile menu
     
 })
