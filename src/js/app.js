41b3d2b1e0ba20b304b8bf25a325f33de31d20fd
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-18 13:26:53 +0200

    sliders updates

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,17 @@ $(document).ready(function(){
     var currentSlide = 1;
     console.log();
 
+    var firstLi = slider.children().eq(0);
+    var cloneFirstLi = firstLi.clone();
+    slider.append(cloneFirstLi);
+
     var intervalId = setInterval(function(){
         slider.animate({'margin-left': '-=100%'},animationSpeed, function(){
             currentSlide++;
-            
+            if(currentSlide === slider.children().length){  
+                slider.css('margin-left',0);
+                 currentSlide = 1;
+            }
         });
     }, 4000)
     
