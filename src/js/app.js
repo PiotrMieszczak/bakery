9ef6124bdd11dbc2a6eb5d560888a927c8edd520
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-18 11:38:51 +0200

    slider update

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,18 @@
 $(document).ready(function(){
 
-    var slider = $('.slider_wraper')
+    var slider = $('.slider')
+    var animationSpeed = 1000; //ms
+    var counter = 1;
+    console.log(slider);
 
-    var slider_width = slider.width();
-    var slider_height = slider.height();
-
-   
-    
-    var img_width = slider.find('img').eq(0).width();
-    var img_height = slider.find('img').eq(0).height()
-    
-   
-
-    slider.css('wdith:'+img_width);
-    slider.css('height:'+img_height);
-
-    $(window).resize(function(){
-        slider.css('width:'+img_width);
-        slider.css('height:'+img_height);
+    // var intervalId = setInterval(function(){
+    //     counter++;
         
-         console.log('slider: ',slider_width, slider_height)
-         console.log('img: ',img_width, img_height);
-    })
+    //     slider.animate({'margin-left': '-=100%'},animationSpeed);
+       
+
+    // }, 1000)
+    
+
 
 })
